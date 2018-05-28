00dc3fcd997976c0c895d851986287a7f971ac58
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-28 14:03:36 -0400

    feat: successfull cobrandl login post request

--- server.rb
+++ server.rb
@@ -1,11 +1,38 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'dotenv/load'
+require 'json'
+require 'rest-client'
 require 'pry'
 
 set :bind, '0.0.0.0'
 
+REST_URL = "https://developer.api.yodlee.com/ysl/restserver/v1/"
+COBRAND_LOGIN_URL = "cobrand/login"
+
+COBRAND_LOGIN = ENV['COBRAND_LOGIN']
+COBRAND_PASS = ENV['COBRAND_PASS']
+LOGIN_NAME = ENV['LOGIN_NAME']
+LOGIN_PASS = ENV['LOGIN_PASS']
+
+def cobrand_login(payload)
+  url = REST_URL + COBRAND_LOGIN_URL
+  RestClient.post(url,
+    payload.to_json,
+    {content_type: :json,accept: :json}
+  )
+end
+
 get '/yodlee-fastlink' do
-  @env_test = ENV['TEST_LOREM']
+  cobrand_login_payload = {
+    "cobrand": {
+      "cobrandLogin": COBRAND_LOGIN,
+      "cobrandPassword": COBRAND_PASS,
+      "locale": "en_US"
+    }
+  }
+  cobrand_login_result = cobrand_login(cobrand_login_payload)
+  #cobrand_login_result.body
+
   erb :index
 end
