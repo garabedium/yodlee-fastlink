9d6fd52b904a18ddf9bfede7fc5e54e220ba51b6
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-28 12:36:24 -0400

    chore: .env vars setup

--- server.rb
+++ server.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'dotenv/load'
 require 'pry'
 
 set :bind, '0.0.0.0'
 
-get '/' do
+get '/yodlee-fastlink' do
+  @env_test = ENV['TEST_LOREM']
   erb :index
 end
